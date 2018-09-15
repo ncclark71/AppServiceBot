ce0ea179e5186a69aeff1ef827851451e508ac4a
ncclark71 <31668208+ncclark71@users.noreply.github.com>
2018-09-14 20:03:45 -0500

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=appserviceqna-Web-Deploy.pubxml -p:Password=cXkvcRdBxMfubmX2KmkppgMajEoJEg6NJg5oGpCw0iahm3Xpl49qlv8FfiPT
+
